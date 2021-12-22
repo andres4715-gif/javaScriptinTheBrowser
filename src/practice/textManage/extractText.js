e1055060444563cd07f53069ad55aad13abdcbc9
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2021-12-22 11:26:37 -0500

    new test cases for text

---
+++ src/practice/textManage/extractText.js
@@ -0,0 +1,10 @@
+const text = "Results (123456789)";
+const str2 = text.substring(9, text.length - 1);
+console.log(str2);
+
+const text2 = "SEE 2885 RESULTS";
+const str3 = text2.substring(0, text2.length - 8);
+const str4 = str3.substring(4, text2.length - 0);
+
+// console.log(str3);
+console.log(str4);
