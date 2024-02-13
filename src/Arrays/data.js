c8f7710f08495604878d560f13eeb4e8655c928e
Andres Rios <andres4715@gmail.com>
2024-02-13 06:57:13 -0500

    new data

---
+++ src/Arrays/data.js
@@ -0,0 +1,23 @@
+export const family = [
+    {
+        firstName: "Andres",
+        lastName: "Rios", 
+        age: 42, 
+        children: 0, 
+        phone: 9898787654, 
+    }, 
+    {
+        firstName: "Daniel",
+        lastName: "Rios", 
+        age: 36, 
+        children: 2,    
+        phone: 123123123, 
+    }, 
+    {
+        firstName: "Liliana",
+        lastName: "Rios", 
+        age: 48, 
+        children: 1, 
+        phone: 123456786, 
+    }
+]
